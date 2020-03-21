834ab5756e8b8f8de88f6d6f43c74eb596a6765f
frdnndvn106@gmail.com <frdnndvn106@gmail.com>
2020-03-21 18:34:28 +0700

    Add comment

--- routes/post.js
+++ routes/post.js
@@ -3,12 +3,15 @@ const PostController = require("../controllers/PostController");
 const CommentController = require("../controllers/CommentController");
 
 var router = express.Router();
+
+//Routes for Post CRUD
 router.get("/", PostController.postList);
 router.get("/:id", PostController.postDetail);
 router.post("/", PostController.postStore);
 router.put("/:id", PostController.postUpdate);
 router.delete("/:id", PostController.postDelete);
 
+//Routes for Comment CRUD
 router.get("/:forPost/comments", CommentController.commentList);
 router.post("/:forPost/comments", CommentController.commentStore);
 router.put("/:forPost/comments/:id", CommentController.commentUpdate);
